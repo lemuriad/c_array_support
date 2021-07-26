f12bbfc8314c6dcc34e468a9f26d92943595ceff
Will Wray <wjwray@gmail.com>
2021-07-26 11:25:27 -0400

    assign design and impl overhaul, add tests

--- array_assign.hpp
+++ array_assign.hpp
@@ -11,53 +11,73 @@
   array_assign.hpp
  -=================-
 
-  Concepts and functor for generic assignment including C-array.
+  Concepts, class template and function for making assignment generic,
+  primarily to facilitate C-array assignment (until such a time that C++
+  language rules are relaxed to allow more array copy semantics).
 
   Avoids <algorithm> or <functional> dependency; implements algorithms
   equivalent to std::copy, for possibly nested arrays (currently lacks
   optimizations such as memcpy specializations).
 
+  Generic assignment is also useful for defining extended assignment
+  outside of class scope (as operator=(R) has to be a member function).
+  For example, tuple assignment allows implicit conversions of elements,
+  and has many special assignments which bloat its API and are better
+  defined externally with a means for extension by the user.
+
   Concepts:
 
     ltl::assignable_from         c.f. std::assignable_from
-    ltl::default_assignable      (no std equivalent)
+    ltl::default_assignable      see below (no std equivalent)
+    ltl::assign_toable           see below (no std equivalent)
 
     default_assignable<T> captures language concept that a value can be
-    assigned from an empty braced initializer list: v = {};
+    'zero-init' assigned from an empty braced initializer list: v = {};
+    as implied by default_initializable<T> && assignable_from<T&, T>,
+    though class types can also model the concept with an operator=({}).
 
-    It's implied by default_initializable<T> && assignable_from<T&, T>.
-    Class types can also model the concept with an operator=({}).
+  Class template:
+    ltl::assign_to<T> customization point for users to specialize,
+                      with operator=(R) overloads
+  Function:
+    ltl::assign(l,r) assign r to l
+                 (r may be a braced init list, empty for default-init)
 
-  Functors:
-    ltl::assign
+    ltl::assign(l) return assign_to<L> if extant else return l reference
 
   Usage
   =====
 
+    int a[2], b[2];
+    ltl::assign(a) = {1,2}; // A reference-wrapper fpr assignment
+    ltl::assign(b,{3,4});
+    ltl::assign_to{a} = b;
+    ltl::assign(a) = {};
+
     template <ltl::default_assignable...T>
-    void clear(T&&...v) { (ltl::assign{}((T&&)v), ...); }
+    void clear(T&&...v) { (ltl::assign((T&&)v,{}), ...); }
 
     template <typename L, typename R>
       requires ltl::assignable_from<L&,R&&>
-    L& ass(L& l, R&& r) {
-      return ltl::assign{}(l,(R&&)r);
-    }
+    L& ass(L& l, R&& r) { return ltl::assign(l,(R&&)r); }
   
   See the ltl::tupl implementation for its use in array member support.
 
   Raison d'etre
   =============
-   C-array doesn't have copy semantics; in particular assignment:
+  This header is a 'polyfill' for lack of generic assignment support,
+  in the language or standard library.
+
+  C-array doesn't have copy semantics; in particular assignment:
 
       int a[2];
       int b[2]{};
       a = b;    // error: array type 'int [2]' is not assignable
       a = {};   // error: assigning to an array from an initializer list
-      a = {1,2};
+      a = {1,2}; // ""
 
    P1997 fixes this. In the meantime, C++20 doesn't have std facilities
-   for generic assignment, as for comparisons (see array_compare.hpp),
-   so this header attempts a similar 'polyfill'.
+   for generic assignment, as it has for comparisons.
 */
 
 #include <concepts>
@@ -68,65 +88,94 @@
 
 template <typename T>
 concept default_assignable =
-          requires(flat_element_t<T> v) { (T)v={}; };
+  requires (flat_element_t<T> v) {
+    static_cast<flat_element_t<T>>(v) = {};
+  };
 
 template <typename L, typename R>
 concept assignable_from =
    std::assignable_from< flat_element_t<L>, flat_element_t<R> >
 && same_extents_v<std::remove_cvref_t<L>,std::remove_cvref_t<R>>;
 
-// assign functor, includes array assignment unlike core language op=
-// assign(l) single-argument form is default-assign ={} (element-wise)
-template <typename L = void> struct assign_to {};
+// assign_to functor, customization point
+// invoked by assign(l) function for assign_toable types
+template <typename L> struct assign_to {};
+template <typename L> assign_to(L&&) -> assign_to<L&&>;
 
-template <typename L> concept assign_toable = requires {typename assign_to<L>::value_type; };
+template <typename L> concept assign_toable =
+  requires { typename assign_to<L>::value_type; };
 
-template <c_array L> struct assign_to<L>
+// assign_to specialization for array assignment
+// (note: operator=(R) -> L& always returns wrapped type, not wrapper)
+template <c_array L>
+struct assign_to<L>
 {
-    L& l;
+  L& l;
 
-    using value_type = std::remove_reference_t<L>;
+  using value_type = std::remove_reference_t<L>;
+  using remove_extent_t = std::remove_extent_t<value_type>;
 
-    constexpr assign_to const operator=(assign_to<>) const
-        noexcept(noexcept(flat_index(l) = {}))
-        requires default_assignable<L&>
-    {
-        for (int i = 0; i != flat_size<L>; ++i)
-            flat_index(l, i) = {};
-        return *this;
-    }
+  // operator=({}) overload for emtpy braced-init
+  constexpr L& operator=(assign_to<void>) const
+      noexcept(noexcept(flat_index(l) = {}))
+    requires default_assignable<L&>
+  {
+      for (int i = 0; i != flat_size<L>; ++i)
+          flat_index(l, i) = {};
+      return l;
+  }
 
-    template <typename R>
+  // operator=(lval) overload for array lvalues (and rvalue variables)
+  template <c_array R>
     requires assignable_from<L, R&&>
-        && same_extents_v<value_type, std::remove_cvref_t<R>>
-        constexpr assign_to const operator=(R&& r) const
-        noexcept(noexcept(flat_index(l) = flat_index((R&&)r)))
-    {
-        for (int i = 0; i != flat_size<L>; ++i)
-            flat_index(l, i) = flat_index((R&&)r, i);
-        return *this;
-    }
+          && same_extents_v<value_type, std::remove_cvref_t<R>>
+  constexpr L& operator=(R&& r) const
+      noexcept(noexcept(flat_index(l) = flat_index((R&&)r)))
+  {
+      for (int i = 0; i != flat_size<L>; ++i)
+          flat_index(l, i) = flat_index((R&&)r, i);
+      return l;
+  }
 
-    template <int N>
+  // operator=(rval) overload for array rvalue from braced-init
+  template <int N>
     requires (N == std::extent_v<value_type>)
-        constexpr assign_to const operator=(std::remove_extent_t<value_type>const(&r)[N]) const
-        noexcept(noexcept(flat_index(l) = flat_index(r)))
-    {
-        return operator=<value_type const&>(r);
-    }
+  constexpr L& operator=(remove_extent_t const(&r)[N]) const
+      noexcept(noexcept(flat_index(l) = flat_index(r)))
+  {
+      return operator=<value_type const&>(r);
+  }
 
 };
-template <typename L> assign_to(L&&)->assign_to<L&&>;
 
-template <typename T>
-std::remove_reference_t<T> const& as_const(T&& v) { return v; }
-
+// assign(l) returns assign_to{l}, if assign_toable, else reference-to-l
 template <typename L>
 decltype(auto) assign(L&& l) {
     if constexpr (assign_toable<L&&>)
         return std::type_identity_t<assign_to<L&&> const>{l};
     else
         return (L&&)l;
+}
+
+template <typename L, typename R>
+decltype(auto) assign(L&& l, R&& r) {
+    if constexpr (assign_toable<L&&>)
+        return std::type_identity_t<assign_to<L&&> const>{l} = (R&&)r;
+    else
+        return l = (R&&)r;
+}
+
+template <typename L>
+decltype(auto) assign(L&& l, std::remove_cvref_t<L> const& r) {
+    if constexpr (assign_toable<L&&>)
+        return std::type_identity_t<assign_to<L&&> const>{l} = r;
+    else
+        return l = r;
+}
+
+template <typename E, int N>
+decltype(auto) assign(E(&l)[N], E const(&r)[N]) {
+    return std::type_identity_t<assign_to<E(&)[N]> const>{l} = r;
 }
 
 #include "namespace.hpp"
--- tests/test_array_assign.cpp
+++ tests/test_array_assign.cpp
@@ -2,7 +2,95 @@
 
 #include <cassert>
 
-int main() {
+bool test_assign_to_array1D()
+{
+  int a[2], b[2];
+  ltl::assign_to{a} = {1,2};
+  ltl::assign_to{b} = a;
+  assert( a[0] == 1 && b[0] == 1
+       && a[1] == 2 && b[1] == 2);
+  ltl::assign_to{b} = {};
+  assert( b[0] == 0 && b[1] == 0 );
+
+  return true;
+}
+
+bool test_assign_to_array2D()
+{
+  int a[3][2], b[3][2];
+  ltl::assign_to{a} = {{1,2},{3,4},{5,6}};
+  ltl::assign_to{b} = a;
+  assert( a[0][0] == 1 && b[0][0] == 1
+       && a[1][0] == 3 && b[1][0] == 3);
+  ltl::assign_to{b} = {};
+  assert( b[0][0] == 0 && b[1][0] == 0 );
+  ltl::assign_to{b[0]} = a[2];
+  assert( b[0][0] == 5 && b[0][1] == 6 );
+
+  return true;
+}
+
+bool test_assign_scalar()
+{
+  int a, b;
+  ltl::assign(a) = 1;
+  ltl::assign(b) = {1};
+  assert( a == 1 && b == 1 );
+  ltl::assign(a) = 0;
+  ltl::assign(b) = {};
+  assert( a == 0 && b == 0 );
+  ltl::assign(a,1);
+  ltl::assign(b,{1});
+  assert( a == 1 && b == 1 );
+  ltl::assign(b,{});
+  assert( b == 0 );
+
+  return true;
+}
+
+bool test_assign_array1D()
+{
+  int a[2], b[2];
+  ltl::assign(a) = {1,2};
+  ltl::assign(b) = a;
+  assert( a[0] == 1 && b[0] == 1
+       && a[1] == 2 && b[1] == 2);
+  ltl::assign(b) = {};
+  assert( b[0] == 0 && b[1] == 0 );
+
+  ltl::assign(a,{3,4});
+  ltl::assign(b,a);
+  assert( a[0] == 3 && b[0] == 3
+       && a[1] == 4 && b[1] == 4);
+  ltl::assign(b,{});
+  assert( b[0] == 0 && b[1] == 0 );
+
+  return true;
+}
+
+bool test_assign_array2D()
+{
+  int a[3][2], b[3][2];
+  ltl::assign(a) = {{1,2},{3,4},{5,6}};
+  ltl::assign(b) = a;
+  assert( a[0][0] == 1 && b[0][0] == 1
+       && a[1][0] == 3 && b[1][0] == 3);
+  ltl::assign(b,{});
+  assert( b[0][0] == 0 && b[1][0] == 0 );
+  ltl::assign(b[0],a[2]);
+  assert( b[0][0] == 5 && b[0][1] == 6 );
+
+  return true;
+}
+
+int main()
+{
+  test_assign_to_array1D();
+  test_assign_to_array2D();
+  test_assign_scalar();
+  test_assign_array1D();
+  test_assign_array2D();
+
   wrap<int> wi{2};
   auto& [wiv] = wi;
   assert( wiv == 2 );
